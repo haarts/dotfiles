13450874bd0b5d00783025ffd1fadf79aa24f25b
Harm Aarts <harm@aarts.email>
2019-06-30 14:58:46 +0200

    Save some fish functions

---
+++ fish/b.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.sRf6GW/b.fish @ line 2
+function b
+	curl -s https://api.coindesk.com/v1/bpi/currentprice/EUR.json | jq .bpi.EUR.rate
+end
---
+++ fish/l.fish
@@ -0,0 +1,3 @@
+function l
+  exa -l $argv
+end
