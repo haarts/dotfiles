e104696aa025c3bb604c0da2cf695ba4a1b67c2c
Harm Aarts <harm@aarts.email>
2019-12-27 22:19:17 +0100

    Split long line

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,20 @@
 set fish_greeting
-set -xg PATH $PATH /usr/local/go/bin
 set -xg GOPATH $HOME/prj/go
+set -xg RUSTPATH $HOME/.cargo
+set -xg PATH \
+	$HOME/local/julia/bin \
+	$RUSTPATH/bin \
+	$GOPATH/bin \
+	$HOME/.gem/ruby/2.5.0/bin \
+	$HOME/.local/bin \
+	$HOME/local/bin \
+	$HOME/.pub-cache/bin \
+	$HOME/local/Android/Sdk/platform-tools \
+	$HOME/local/android-studio/bin \
+	$HOME/local/flutter/bin/cache/dart-sdk/bin \
+	$HOME/local/flutter/bin \
+	/usr/local/go/bin \
+	$PATH
 set -xg EDITOR kak
 alias o 'xdg-open'
 
